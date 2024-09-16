4b0b8e6990e7c7be434bd44e4847a8aba887c8e5
Mounikadevloper <157671994+Mounikadevloper@users.noreply.github.com>
2024-09-16 21:23:54 +0530

    Delete agents/# agents directory

--- agents/# agents/tool_agent.py
+++
@@ -1,9 +0,0 @@
-# agents/tool_agent.py
-class ToolAgent:
-    def __init__(self, tools):
-        self.tools = tools
-
-    def execute_task(self, task):
-        for tool in self.tools:
-            result = tool.apply_task(task)
-            return result
